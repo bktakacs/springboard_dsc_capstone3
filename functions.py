6179db4e9834b3e4f3304b220d490c943bbc3306
Bence Takacs <dtn125@alumni.ku.dk>
2025-03-04 10:11:36 -0500

    renamed load_data function to loadtabledata and added load_csv_ function

--- functions.py
+++ functions.py
@@ -5,17 +5,31 @@ import os
 datapath = './data/'
 
 # Function to load and preprocess data
-def load_data(filename, verbose: bool = True):
-    df = pd.read_csv(datapath + filename)
-    df['Month'] = pd.to_datetime(df['Month'])
-    df.set_index('Month', inplace=True)
-    for col in df.columns:
-        df[col] = pd.to_numeric(df[col], errors='coerce')
+def load_table_data(filename):
+    try:
+        df = pd.read_csv(datapath + filename)
+        df['Month'] = pd.to_datetime(df['Month'])
+        df.set_index('Month', inplace=True)
+        for col in df.columns:
+            df[col] = pd.to_numeric(df[col], errors='coerce')
+        
+        print('Data loaded from: {}'.format(filename))
+    
+    except:
+        print('Error loading data from: {}'.format(filename))
+        df = None
 
-    if verbose:
-        # print(f'\n\nData loaded from: {filename}', df.isnull().sum())
-        print('\n\nData loaded from: {}\n{}\n\n'.format(
-            filename, df.isnull().sum
-        ))
+    return df
+
+def load_csv_data(filename):
+    try:
+        df = pd.read_csv(datapath + filename)
+        df['Month'] = pd.to_datetime(df['Month'])
+        df.set_index('Month', inplace=True)
+        print('Data loaded from: {}'.format(filename))
+    
+    except:
+        print('Error loading data from: {}'.format(filename))
+        df = None
 
     return df
